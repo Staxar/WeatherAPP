4498511f21fff256100fc632cd769912348270be
Marcin <brzeszczot90@gmail.com>
2024-01-25 09:23:21 +0100

    connect with backend

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImageBackground, SafeAreaView, StyleSheet, View } from "react-native";
 
 import fakeData from "../assets/data.json";
 import Header from "../components/Header";
 import WheaterBaseInfo from "../components/WheaterBaseInfo";
 
+interface WeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
 export default function Page() {
-  const [data, setData] = useState(fakeData);
+  const [data, setData] = useState<WeatherResponse>(fakeData);
+  const [cityName, setCityName] = useState<string>("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const queryParams = new URLSearchParams();
+      queryParams.append("units", "metric");
+      const response = await fetch(
+        `http://172.16.1.144:3000/weather/warsaw?${queryParams}`,
+      );
+      if (response.ok) {
+        const responseData: WeatherResponse = await response.json();
+        setData(responseData);
+      } else {
+        alert("Something went wrong! Check city name and try again.");
+      }
+    };
+    fetchData();
+  }, [cityName]);
 
   return (
     <SafeAreaView style={styles.outerContainer}>
